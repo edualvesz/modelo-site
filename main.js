41ca2b77cc524690b74778e8687d4671605dd8b9
edualvesz <eduardo_ean@outlook.com>
2022-06-15 14:13:21 -0300

    ajustes javascript e css

--- main.js
+++ main.js
@@ -79,6 +79,26 @@ function backToTop(){
 const sections = document.querySelectorAll('main section[id]') /* selects all sections that contains an id */
 function activateMenuAtCurrentSection(){
 
+  const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4 /* it gonna takes all window size */
+
+  for (const section of sections){
+    const sectionTop = section.offsetTop
+    const sectionHeight = section.offsetHeight
+    const sectionId = section.getAttribute('id')
+
+    const checkpointStart = checkpoint >= sectionTop
+    const checkpointEnd = checkpoint <=sectionTop + sectionHeight
+
+    if(checkpointStart && checkpointEnd) {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.add('active')
+    } else {
+      document
+      .querySelector('nav ul li a[href*=' + sectionId + ']')
+      .classList.remove('active')
+    }
+  }
 }
 
 
